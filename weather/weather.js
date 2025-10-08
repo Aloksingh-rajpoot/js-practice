45bf7990258ca06addd5341ea1d9dda81dd0909d
Alok Singh <aloksingh-rajpoot>
2025-10-08 11:38:01 +0530

    compt weather aap

---
+++ weather/weather.js
@@ -0,0 +1,10 @@
+// const apiKey = "317331e4a4d5d4bf8ced62942ec9ebf2";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&appid="
+
+//   async function checkWeather() {
+//     const response = await fetch(apiUrl +`&appId=${apiKey}`);
+//     let data = await response.json();
+//     console.log(data);
+//   }
+
+//   checkWeather();
